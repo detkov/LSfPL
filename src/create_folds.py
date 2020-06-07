5e950420ce4f51652ec7bb9554a469f8f32211fb
Nikita Detkov <nds-gfk@mail.ru>
2020-06-07 16:11:09 +0500

    Made a script creating folds
    With a bunch of parameters like pct of val. fold, feature, target, etc

--- src/create_folds.py
+++ src/create_folds.py
@@ -4,7 +4,6 @@ import pandas as pd
 import numpy as np
 import sys
 
-FEATURES = 'image_id'
 
 def get_params(args):
     parser = argparse.ArgumentParser(description='Makes a split of your train file.')
@@ -14,8 +13,14 @@ def get_params(args):
                         dest='output', default='../input/train_folds.csv')
     parser.add_argument('-s', '--splits', help='Number of splits.', 
                         dest='splits', type=int, default=5)
+    parser.add_argument('-v', '--valid', help='Percent of data to be hold-outed for validation (Ensembling, WAE, etc.).', 
+                        dest='valid', type=int, default=0)
     parser.add_argument('-r', '--randomstate', help='random_state parameter value.', 
                         dest='randomstate', type=int, default=42)
+    parser.add_argument('-f', '--feature', help='Unique feature of row, f.e. "image_name".', 
+                        dest='feature')
+    parser.add_argument('-t', '--target', help='Target feature to be splitted by.', 
+                        dest='target', default='target')
     return parser.parse_args(args)
 
 
@@ -23,15 +28,31 @@ if __name__ == "__main__":
     params = get_params(sys.argv[1:])
     df = pd.read_csv(params.input)
     df['kfold'] = np.nan
-    df = df.sample(frac=1).reset_index(drop=True)
 
-    X = df[FEATURES].values
-    y = df.iloc[:, 2] + df.iloc[:, 3] * 2 + df.iloc[:, 4] * 3
+    X = df[params.feature].values
+    y = df[params.target].values
+
+    if params.valid != 0:
+        skf = StratifiedKFold(n_splits=int(100/params.valid), random_state=params.randomstate)
+        for fold, (train, valid) in enumerate(skf.split(X, y)):
+            if fold == 0:
+                df.loc[valid, 'kfold'] = -1
+            else:
+                break
+    df_valid = df[df['kfold'] == -1].reset_index(drop=True)
+
+
+    df_train = df[df['kfold'].isnull()].reset_index(drop=True)
+    X = df_train[params.feature].values
+    y = df_train[params.target].values
 
     skf = StratifiedKFold(n_splits=params.splits, random_state=params.randomstate)
     for fold, (train, valid) in enumerate(skf.split(X, y)):
-        df.loc[valid, 'kfold'] = fold
+        df_train.loc[valid, 'kfold'] = fold
 
+    df = pd.concat([df_train, df_valid])
+    df.reset_index(drop=True, inplace=True)
+    print('Number of samples per class:')
     print(df['kfold'].value_counts(dropna=False))
-    print(df.head())
+    print('\nClass "-1" is for validation')
     df.to_csv(params.output, index=False)
--- src/preprocess.sh
+++ src/preprocess.sh
@@ -1,2 +1,3 @@
-python preprocess.py -w 512 -H 512 -p 8 -x jpg -i ../input/test/ -o ../input/test_resized/
-python preprocess.py -w 512 -H 512 -p 8 -x jpg -i ../input/train/ -o ../input/train_resized/
+python resize_images.py -w 512 -H 512 -p 8 -x jpg -i ../input/test/ -o ../input/test_resized/
+python resize_images.py -w 512 -H 512 -p 8 -x jpg -i ../input/train/ -o ../input/train_resized/
+python create_folds.py -v 10 -f image_name 
