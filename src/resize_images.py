6cdf55ef9d1244c33aa62f79f880aad79296575b
nmramorov <48467471+nmramorov@users.noreply.github.com>
2020-06-09 12:18:44 +0500

    Возможные правки к коду

--- src/resize_images.py
+++ src/resize_images.py
@@ -5,6 +5,7 @@ from PIL import Image
 from glob import glob
 from tqdm import tqdm
 from multiprocessing import Pool
+# Отсортировать зависимости
 
 
 def get_params(args):
@@ -13,6 +14,7 @@ def get_params(args):
                         dest='width', type=int)
     parser.add_argument('-H','--height', help='Height of resized images.', 
                         dest='height', type=int)
+                        # а где дефолтные значения?
     parser.add_argument('-p', '--processes', help='Number of processes to be used.', 
                         dest='processes', type=int, default=8)
     parser.add_argument('-x', '--extention', help='Extention of images.', 
@@ -28,10 +30,13 @@ if __name__ == "__main__":
     params = get_params(sys.argv[1:])
     
     OUTPUT_SIZE = (params.width, params.height)
+    # Я бы распаковал как WIDTH, HEIGHT = params.width, params.height
     INPUT_DIR = params.input
     OUTPUT_DIR = params.output
     N_PROCESSES = params.processes
     EXTENTION = params.extention
+    # Дело твое, я бы не именовал их так - параметры же
+    # задаются динамически. Но это как сам знаешь
     print((f'Resize params: width is {OUTPUT_SIZE[0]}, height is {OUTPUT_SIZE[1]}.'
            f'\nNumber of processes: {N_PROCESSES}.'
            f'\nLooking for .{EXTENTION} images.'))
@@ -45,7 +50,7 @@ if __name__ == "__main__":
         img = Image.open(img_path)
         img = img.resize(OUTPUT_SIZE, Image.BICUBIC)
         img.save(os.path.join(OUTPUT_DIR, os.path.basename(img_path)))
-
+    # Функция в __main__? Зачем? Не православно
 
     with Pool(N_PROCESSES) as p:
         list(tqdm(p.imap(resize_img, pathes), total=len(pathes)))
