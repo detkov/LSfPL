debb637d0019a24aa435a36127dd4777836383a6
Nikita Detkov <nds-gfk@mail.ru>
2020-06-11 04:24:04 +0500

    Created fine-tuning scripts and wrote logic of further aims

--- src/preprocess.sh
+++ src/preprocess.sh
@@ -1,3 +1,4 @@
-python resize_images.py -w 256 -H 256 -p 8 -x jpg -i ../input/jpeg/test/ -o ../input/images_resized_256x256/
-python resize_images.py -w 256 -H 256 -p 8 -x jpg -i ../input/jpeg/train/ -o ../input/images_resized_256x256/
-python create_folds.py -v 10 -f image_name 
+python resize_images.py -w 512 -H 512 -p 8 -x jpg -i ../input/jpeg/test/ -o ../input/images_resized_512x512/
+python resize_images.py -w 512 -H 512 -p 8 -x jpg -i ../input/jpeg/train/ -o ../input/images_resized_512x512/
+python create_folds_stratified.py -v 10 -f image_name 
+python create_folds_groups.py -v 10 -f image_name -f patient_id
